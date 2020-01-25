72d148159f17646e56ebb3c531cd0f3876f2c41f
Davit Margaryan <davitmargaryan1987@gmail.com>
2020-01-25 16:44:21 +0400

    OK

---
+++ script.js
@@ -0,0 +1,13 @@
+const row = 20;
+const col = 10;
+const vacant;
+
+let board = [];
+
+for(let r=0; r < row; r++){
+    board[r] = [];
+    for(let c=0; c < col; c++){
+        board [r][c] = vacant;
+    }
+}
+
